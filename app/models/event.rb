5740c17994d4d1e3ce611cfe5b6914566b6415f9
Rohan James <rohanjames9282@gmail.com>
2023-02-08 20:18:11 +0530

    Add validations for additional columns in event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,11 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: :User, foreign_key: :creator_id
+  validates :title, length: { in: 1..15 }, presence: true
+  validates :date, presence: true
+  validates :desc, length: { in: 20..250 }, presence: true
+  validates :country, length: { in: 3..56 }, presence: true
+  validates :city, length: { in: 3..85 }, presence: true
+  validates :starting_time, presence: true
+  validates :ending_time, presence: true
+  validates_associated :creator
 end
