1c602a6d4dbdc661bad2faaa9f97883df9cb4a8e
Rohan James <rohanjames9282@gmail.com>
2023-02-08 19:53:19 +0530

    Add #new and #create to events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,27 @@
 class EventsController < ApplicationController
+  before_action :user_signed_in?, only: [:new, :create]
 
   def index
     @events = Event.all
+  end
+
+  def new
+    @event = current_user.events.build
+  end
+
+  def create
+    @event = current_user.events.new(event_params)
+
+    if @event.save
+      redirect_to root_url
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :date, :desc, location: [:country, :city], timing: [:starting_time, :ending_time])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "events#index"
-  resources :events, only: [:index]
+  resources :events, only: [:index, :new, :create]
   resources :users, only:[:show]
 end
