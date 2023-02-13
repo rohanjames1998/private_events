b9c544a4f5b81dd47c48fe4f26fa3f6fe6fefbb3
Rohan James <rohanjames9282@gmail.com>
2023-02-13 19:14:06 +0530

    Add routes for invitation

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   root "events#index"
   resources :events, only: [:index, :new, :create, :show]
   resources :users, only:[:show]
+  resources :invitations, only:[:create]
 end
