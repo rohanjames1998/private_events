35d1f0a4d08ce2b7177e2308a84342f5523a8a2a
Rohan James <rohanjames9282@gmail.com>
2023-02-13 19:11:42 +0530

    Add check to avoid multiple instances of same user as attendee

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,4 +1,6 @@
 class Invitation < ApplicationRecord
   belongs_to :attendee, class_name: :User
   belongs_to :attended_event, class_name: :Event
+
+  validates :attendee_id, uniqueness: { scope: :attended_event_id, message: "You are already invited to this event." }
 end
