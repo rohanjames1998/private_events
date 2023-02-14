4393e6228696d9043369809539cc172efbbb3a5d
Rohan James <rohanjames9282@gmail.com>
2023-02-14 16:18:06 +0530

    Update routes for events

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "events#index"
-  resources :events, only: [:index, :new, :create, :show]
+  resources :events
   resources :users, only:[:show]
   resources :invitations, only:[:create]
 end
