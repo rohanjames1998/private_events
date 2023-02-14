07450f6253ab7955fe9f61d0fbe424e4129822db
Rohan James <rohanjames9282@gmail.com>
2023-02-14 21:31:22 +0530

    Fix foreign key constraint error when deleting user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :events, foreign_key: :creator_id
-  has_many :invitations, foreign_key: :attendee_id
-  has_many :attended_events, through: :invitations
+  has_many :events, foreign_key: :creator_id, dependent: :destroy
+  has_many :invitations, foreign_key: :attendee_id, dependent: :destroy
+  has_many :attended_events, through: :invitations, dependent: :destroy
 
   validates :first_name, length: { in: 3..10 }, presence: true
   validates :last_name, length: { in: 3..10 }, presence: true
